06c6b038e07425033e83cffa3d6892c2d2801685
marielleo <89753621+marielleo@users.noreply.github.com>
2021-09-05 19:00:11 -0400

    Update timeloop.cs

--- timeloop.cs
+++ timeloop.cs
@@ -1,7 +1,9 @@
-echo "# time-loop-" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/marielleo/time-loop-.git
-git push -u origin main
+using System;
+
+class timeloop {
+    static void Main (string[] args) {
+        int num = int.Parse (Console.ReadLine ());
+        for (int x = 1; x <= num; x++)
+            Console.WriteLine (x + " Abracadabra");
+    }
+}
